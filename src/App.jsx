ae7f001d236c562fda0f39e346dc3aadcb0b395c
maelle <maelle.r.baudot@gmail.com>
2026-04-16 15:59:51 +0200

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-
 import './App.css'
+import { useEffect } from 'react';
+
 import { Accueil } from './Portfolio/1_accueil';
 import NavBar from './Portfolio/0_NavBar';
 import { Parcours } from './Portfolio/2_parcours';
@@ -7,52 +8,38 @@ import { Compétences } from './Portfolio/3_compétences';
 import { Projets } from './Portfolio/4_projets'
 import { Contact } from './Portfolio/5_contact';
 
-
-
-
 function App() {
-
 
   return (
     <div className="App">
 
+  
       <div className="gradient-bg">
-
         <div className="gradient-container">
           <div className="g1"></div>
           <div className="g2"></div>
           <div className="g3"></div>
           <div className="g4"></div>
           <div className="g5"></div>
+          <div className="interactive"></div>
         </div>
-    </div>
+      </div>
 
-      
+    
       <div className='content'>
-  {/* <NavBar /> */}
-  <NavBar />
-        {/* <Accueil /> */}
+        <NavBar />
         <Accueil />
-
-        {/* <Parcours /> */}
         <Parcours />
-
-        {/* <Compétences /> */}
         <Compétences />
-
-        {/* <Projets /> */}
         <Projets />
 
-      
-      {/* <Contact /> */}
+        <footer>
+          <Contact />
+        </footer>
+      </div>
 
-      <footer>
-        <Contact />
-      </footer>
-
-</div>
     </div>
   );
 }
 
-export default App
+export default App;
